71bf1a096c1c5ff92f315bb93a52dea1e25ebf24
Domagoj Bunoza <domagoj.bunoza@rise-world.com>
2023-07-06 19:50:54 +0200

    Initial Commit

---
+++ DictaScore/DictaScoreApp.swift
@@ -0,0 +1,14 @@
+// Urheberrechtshinweis: Diese Software ist urheberrechtlich geschützt. Das Urheberrecht liegt bei
+// Research Industrial Systems Engineering (RISE) Forschungs-, Entwicklungs- und Großprojektberatung GmbH,
+// soweit nicht im Folgenden näher gekennzeichnet.
+
+import SwiftUI
+
+@main
+struct DictaScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
